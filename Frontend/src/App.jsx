ef01281c62323a742372d6b6fa5539ff03c06f01
Lukachiti <chitidze.luka@outlook.com>
2026-06-09 21:32:46 +0400

    fix it pls

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import AdminDashboard from "./AdminDashboard";
-import Cart from "./cart";
+import Cart from "./Cart";
 import AuthModal from "./AuthModal";
 import { Link } from "react-router-dom";
 const CATEGORIES = [
--- Frontend/src/Cart.jsx
+++ Frontend/src/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./cart.css";
+import "./Cart.css";
 
 function Cart({ token, onCartItemDeleted }) {
   const [cartItems, setCartItems] = useState([]);
